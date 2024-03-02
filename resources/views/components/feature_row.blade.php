5a1abfdca1839f73eed97bdf419cd55d99a10f93
Evgenii Dianov <dianov.js@gmail.com>
2024-03-02 19:57:56 +0300

    FIX: Feature row

--- resources/views/components/feature_row.blade.php
+++ resources/views/components/feature_row.blade.php
@@ -5,7 +5,7 @@
         <div class="feature-row__wrap">
             <div class="feature-row__top">
                 <div class="feature-row__num">{{ $num }}</div>
-                <div>
+                <div class="feature-row__right">
                     <h3 class="feature-row__title">{{ $title }}</h3>
                     <x-ui.plus class="ml-auto" />
                 </div>
