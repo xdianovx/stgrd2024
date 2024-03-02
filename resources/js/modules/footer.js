d4e30f6dd1af6c3812a903c446665e7bfefacd0b
Evgenii Dianov <dianov.js@gmail.com>
2024-03-02 15:14:10 +0300

    Remove markers

--- resources/js/modules/footer.js
+++ resources/js/modules/footer.js
@@ -2,7 +2,6 @@ export const footer = (ScrollTrigger) => {
     ScrollTrigger.create({
         trigger: ".footer",
         pin: true,
-        markers: true,
         start: "bottom bottom",
         end: "+=100%",
     });
