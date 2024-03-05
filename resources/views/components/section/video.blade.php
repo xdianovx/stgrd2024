53d4ee72394af04cec2f78b195d958d686fe9545
Evgenii Dianov <dianov.js@gmail.com>
2024-03-05 20:01:31 +0300

    ADD: Video test

--- resources/views/components/section/video.blade.php
+++ resources/views/components/section/video.blade.php
@@ -8,7 +8,7 @@
 </section>
 <div class="showreelmodal">
     <video autoplay loop playsinline muted class="showreel-short" data-cursor-text="Close!">
-        <source src="{{ asset('video/2.webm') }}" type="video/webm" />
+        <source src="{{ asset('video/1.mp4') }}" type="video/mp4" />
     </video>
 
     <div class="settings">
